d311ea765457275e2485627146a5e7794ecf35ab
Radim Daniel Panek <rdpanek@gmail.cz>
2020-01-02 23:17:49 +0100

    performanceTesting/smoke.js: upgrade: added waitForLoadEventEnd by new service at wpt2-demo:d.2.18

--- test/performanceTesting/smoke.js
+++ test/performanceTesting/smoke.js
@@ -2,14 +2,14 @@ describe('Performance Testing', () => {
 
   it('open', () => {
     // open browser
-    browser.url('https://www.performance-testing.cz')
+    browser.url('/')
+
+    // wait for loadEventEnd
+    browser.waitForloadEventEnd()
 
     // wait for title element
     const titleElm = $('//title[contains(text(),"Performance Testing školení | Web Performance Testing")]')
     expect(titleElm.waitForExist()).to.be.true
-
-    // wait for performance entries
-    browser.waitForPerformanceEntry("favicon.ico")
   })
 
 })
